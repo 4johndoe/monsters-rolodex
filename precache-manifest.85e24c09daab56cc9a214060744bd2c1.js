9e43f28d748582b3a1f06dbe08b4de23c59be384
bogdan demchenko <helpdesk@gerchikco.com>
2019-07-10 10:39:48 +0300

    Updates

---
+++ precache-manifest.85e24c09daab56cc9a214060744bd2c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996b8e2e707436b8a1693b606c273662",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "815d3b23785cfb59ca02",
+    "url": "/monsters-rolodex/static/css/main.c2e66fc7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "815d3b23785cfb59ca02",
+    "url": "/monsters-rolodex/static/js/main.c86ac134.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
